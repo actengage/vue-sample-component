3eac70bb92585d6c6dd0771e138e99f1b2ef9815
Justin Kimbrell <jkimbrell@actengage.com>
2018-11-12 11:40:35 -0700

    0.10.1

--- dist/vue-sample-component.js
+++ dist/vue-sample-component.js
@@ -1,8 +1,8 @@
 /**
  * vue-sample-component
  *
- * 0.10.0
- * 2018-11-07
+ * 0.10.1
+ * 2018-11-12
  */
 
 (function (global, factory) {
@@ -30,6 +30,81 @@
 
     };
 
+    function normalizeComponent(compiledTemplate, injectStyle, defaultExport, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, isShadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+        if (typeof isShadowMode === 'function') {
+            createInjectorSSR = createInjector;
+            createInjector = isShadowMode;
+            isShadowMode = false;
+        }
+        // Vue.extend constructor export interop
+        const options = typeof defaultExport === 'function' ? defaultExport.options : defaultExport;
+        // render functions
+        if (compiledTemplate && compiledTemplate.render) {
+            options.render = compiledTemplate.render;
+            options.staticRenderFns = compiledTemplate.staticRenderFns;
+            options._compiled = true;
+            // functional template
+            if (isFunctionalTemplate) {
+                options.functional = true;
+            }
+        }
+        // scopedId
+        if (scopeId) {
+            options._scopeId = scopeId;
+        }
+        let hook;
+        if (moduleIdentifier) {
+            // server build
+            hook = function (context) {
+                // 2.3 injection
+                context =
+                    context || // cached call
+                        (this.$vnode && this.$vnode.ssrContext) || // stateful
+                        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+                // 2.2 with runInNewContext: true
+                if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                    context = __VUE_SSR_CONTEXT__;
+                }
+                // inject component styles
+                if (injectStyle) {
+                    injectStyle.call(this, createInjectorSSR(context));
+                }
+                // register component module identifier for async chunk inference
+                if (context && context._registeredComponents) {
+                    context._registeredComponents.add(moduleIdentifier);
+                }
+            };
+            // used by ssr in case component is cached and beforeCreate
+            // never gets called
+            options._ssrRegister = hook;
+        }
+        else if (injectStyle) {
+            hook = isShadowMode
+                ? function () {
+                    injectStyle.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+                }
+                : function (context) {
+                    injectStyle.call(this, createInjector(context));
+                };
+        }
+        if (hook) {
+            if (options.functional) {
+                // register for functional component in vue file
+                const originalRender = options.render;
+                options.render = function renderWithStyleInjection(h, context) {
+                    hook.call(context);
+                    return originalRender(h, context);
+                };
+            }
+            else {
+                // inject component registration as beforeCreate hook
+                const existing = options.beforeCreate;
+                options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+            }
+        }
+        return defaultExport;
+    }
+
     /* script */
                 const __vue_script__ = script;
     /* template */
@@ -54,36 +129,13 @@
       const __vue_module_identifier__ = undefined;
       /* functional template */
       const __vue_is_functional_template__ = false;
-      /* component normalizer */
-      function __vue_normalize__(
-        template, style, script$$1,
-        scope, functional, moduleIdentifier,
-        createInjector, createInjectorSSR
-      ) {
-        const component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
-
-        // For security concerns, we use only base name in production mode.
-        component.__file = "/Users/justinkimbrell/Github/vue-sample-component/src/SampleComponent.vue";
-
-        if (!component.render) {
-          component.render = template.render;
-          component.staticRenderFns = template.staticRenderFns;
-          component._compiled = true;
-
-          if (functional) component.functional = true;
-        }
-
-        component._scopeId = scope;
-
-        return component
-      }
       /* style inject */
       
       /* style inject SSR */
       
 
       
-      var SampleComponent = __vue_normalize__(
+      var SampleComponent = normalizeComponent(
         { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
         __vue_inject_styles__,
         __vue_script__,
